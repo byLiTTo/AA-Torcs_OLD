f405b96276eed39a0495c179498ac88db460b00a
bylitto <carlos.garcia642@alu.uhu.es>
2023-06-14 22:39:18 +0200

    Generado Javadoc del Test Driver que controla el sistema de giro

--- src/champ2011client/Client.java
+++ src/champ2011client/Client.java
@@ -9,7 +9,6 @@ import java.util.StringTokenizer;
 
 /**
  * @author Daniele Loiacono
- *
  */
 public class Client {
 
@@ -24,16 +23,15 @@ public class Client {
     private static String trackName;
 
     /**
-     * @param args
-     *            is used to define all the options of the client.
-     *            <port:N> is used to specify the port for the connection (default is 3001)
-     *            <host:ADDRESS> is used to specify the address of the host where the server is running (default is localhost)
-     *            <id:ClientID> is used to specify the ID of the client sent to the server (default is championship2009)
-     *            <verbose:on> is used to set verbose mode on (default is off)
-     *            <maxEpisodes:N> is used to set the number of episodes (default is 1)
-     *            <maxSteps:N> is used to set the max number of steps for each episode (0 is default value, that means unlimited number of steps)
-     *            <stage:N> is used to set the current stage: 0 is WARMUP, 1 is QUALIFYING, 2 is RACE, others value means UNKNOWN (default is UNKNOWN)
-     *            <trackName:name> is used to set the name of current track
+     * @param args is used to define all the options of the client.
+     *             <port:N> is used to specify the port for the connection (default is 3001)
+     *             <host:ADDRESS> is used to specify the address of the host where the server is running (default is localhost)
+     *             <id:ClientID> is used to specify the ID of the client sent to the server (default is championship2009)
+     *             <verbose:on> is used to set verbose mode on (default is off)
+     *             <maxEpisodes:N> is used to set the number of episodes (default is 1)
+     *             <maxSteps:N> is used to set the max number of steps for each episode (0 is default value, that means unlimited number of steps)
+     *             <stage:N> is used to set the current stage: 0 is WARMUP, 1 is QUALIFYING, 2 is RACE, others value means UNKNOWN (default is UNKNOWN)
+     *             <trackName:name> is used to set the name of current track
      */
     public static void main(String[] args) {
         parseParameters(args);
@@ -195,10 +193,8 @@ public class Client {
             System.out.println(name + " is not a class name");
             System.exit(0);
         } catch (InstantiationException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (IllegalAccessException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         return controller;
--- src/mdp/SteerControlVariables.java
+++ src/mdp/SteerControlVariables.java
@@ -4,17 +4,20 @@ import champ2011client.SensorModel;
 
 public class SteerControlVariables {
 
+    //   --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> -->
     public static final String SEPARATOR = ",";
     public static final double INITIAL_VALUE = 0.85;
     public static final double LEARNING_RATE = 0.2;
     public static final double DISCOUNT_FACTOR = 0.8;
     public static final int TRAIN_EPOCHS = 250;
 
+    //   --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> -->
     /* Steering constants*/
     static final float steerLock = (float) 0.785398;
     static final float steerSensitivityOffset = (float) 80.0;
     static final float wheelSensitivityCoeff = 1;
 
+    //   --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> -->
     public static SteerControlVariables.States evaluateSteerState(SensorModel sensorModel) {
         if (sensorModel.getAngleToTrackAxis() < 0) {
             if (sensorModel.getSpeed() > steerSensitivityOffset)
@@ -29,6 +32,7 @@ public class SteerControlVariables {
         }
     }
 
+    //   --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> -->
     public static double steerAction2Double(SensorModel sensorModel, SteerControlVariables.Actions actionPerformed) {
 
         if (actionPerformed != SteerControlVariables.Actions.KEEP_STEERING_WHEEL_STRAIGHT) {
@@ -37,7 +41,9 @@ public class SteerControlVariables {
             float targetAngle = (float) (sensorModel.getAngleToTrackAxis() - sensorModel.getTrackPosition() * 0.5);
             // at high speed reduce the steering command to avoid loosing the control
             if (actionPerformed == Actions.TURN_STEERING_WHEEL_SHARPLY)
-                return (float) (targetAngle / (steerLock * (sensorModel.getSpeed() - steerSensitivityOffset) * wheelSensitivityCoeff));
+                return (float) (targetAngle / (steerLock
+                        * (sensorModel.getSpeed() - steerSensitivityOffset)
+                        * wheelSensitivityCoeff));
             else
                 return (targetAngle) / steerLock;
         }
@@ -45,12 +51,14 @@ public class SteerControlVariables {
         return 0.0;
     }
 
+    //   --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> -->
     public enum Actions {
         KEEP_STEERING_WHEEL_STRAIGHT,
         TURN_STEERING_WHEEL,
         TURN_STEERING_WHEEL_SHARPLY
     }
 
+    //   --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> --> -->
     public enum States {
         STARTING_GRID,
         LEFT_ROAD_AXIS,
